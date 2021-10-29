181e98af91f3c3cfdc18ac4297612c83fc1f35c2
Yevhenii <freshjs.fte@gmail.com>
2021-10-29 20:51:52 +0300

    Add N:N exapmles

--- sql/freelance.sql
+++ sql/freelance.sql
@@ -1,6 +1,7 @@
 /* 
     1 Создать таблицы пользователей, заказов
-    2 Заказы имеют заголовок, описание, емейл автора, рейтинг автора, 
+    2 Заказы имеют заголовок, описание, емейл автора, 
+    рейтинг автора, 
     дата создания, дата обновления,
     3 Ограничения:
         Не пустые поля.
@@ -9,4 +10,23 @@
         Рейтинг автора от 1.0 до 5.0
         Дата создания и обновления дефолтные на сейчас.
         Уникальное сочетание емейла и заголовка
- */
+*/
+
+DROP TABLE "users";
+
+CREATE TABLE "users" (
+    "id" bigserial PRIMARY KEY,
+    "first_name" varchar(128) NOT NULL,
+    "last_name" varchar(128) NOT NULL,
+    "rating" decimal(2, 1) NOT NULL DEFAULT 1.0,
+    "email" varchar(128) NOT NULL UNIQUE
+);
+
+DROP TABLE orders;
+
+CREATE TABLE "orders" (
+    "id" serial PRIMARY KEY,
+    "author_id" bigint NOT NULL,
+    create_date timestamp NOT NULL DEFAULT current_timestamp,
+    FOREIGN KEY ("author_id") REFERENCES "users"("id")
+);
---
+++ sql/shop.sql
@@ -0,0 +1,70 @@
+DROP TABLE users;
+/*  */
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    email varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL
+);
+/*  */
+
+
+
+DROP TABLE orders;
+/*  */
+CREATE TABLE orders (
+    id serial PRIMARY KEY,
+    user_id int,
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+/*  */
+
+
+
+DROP TABLE products;
+/*  */
+CREATE TABLE products (
+    id serial PRIMARY KEY,
+    product_no int NOT NULL UNIQUE,
+    prod_name varchar(255) NOT NULL,
+    price numeric(20, 2) NOT NULL,
+    stock_quantity int NOT NULL
+);
+/*  */
+
+
+
+CREATE TABLE products_to_order (
+    order_id int,
+    prod_id int,
+    quantity int NOT NULL DEFAULT 1,
+    added_time timestamp NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (prod_id) REFERENCES products(id)
+);
+/*  */
+
+
+
+INSERT INTO users (email, last_name)
+VALUES ('test', 'testovich'),
+    ('ivan', 'ivanov');
+/*  */
+INSERT INTO products (product_no, prod_name, price, stock_quantity)
+VALUES 
+(123, 'Iphone', 234, 1000),
+(234, 'Nokia', 234, 1000),
+(345, 'Xiomi', 234, 1000);
+/*  */
+INSERT INTO orders (user_id)
+VALUES (1),
+    (2);
+
+
+
+/*  */
+INSERT INTO products_to_order (order_id, prod_id) VALUES 
+(1, 1),
+(1, 3),
+(2, 2),
+(2, 2);
