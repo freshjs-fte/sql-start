7918540939dc5de3e2d71a632e815bc2ec225bcd
Yevhenii <freshjs.fte@gmail.com>
2021-11-01 21:06:16 +0200

    Add subqueries

--- sql/select.sql
+++ sql/select.sql
@@ -40,5 +40,34 @@ WHERE "birthdate" + interval '18 years' < current_date;
 /*  */
 SELECT * FROM "users"
 WHERE age("birthdate") > interval '18 years';
+/*  */
 
+/* Подзапросы */
+/* Переименовать колонку в результате */
+SELECT Birthday
+FROM
+(
+    SELECT birthdate AS "Birthday"
+    FROM users
+) 
+AS birthdays
+WHERE "birthdays"."Birthday" > '2000.01.01';
+
+/* 
+    Поиск по дате регистрации
+    Фильтрация после 2010 года
+ */
+/* По дате рождения найти пользователей возрастом между 18 и 24 годами */
+SELECT * 
+FROM
+(
+    SELECT * 
+    FROM "users" 
+    WHERE age("birthdate") > '18 years' 
+)
+AS "over18"
+WHERE age("birthdate") < '24 years';
+/* По полному имени найти польз с длиной полн. им. больше 7 символов */
+
+/* По gender найти пользователей женского пола и с определенной фамилией */
 
