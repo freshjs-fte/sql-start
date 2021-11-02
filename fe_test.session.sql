9d4fc037311d4512a39a4b68f2cb127949f0cb40
Yevhenii <freshjs.fte@gmail.com>
2021-11-02 20:56:53 +0200

    Add DML limits (subquery)

--- fe_test.session.sql
+++ fe_test.session.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (
     "isMale" boolean NOT NULL,
     birthday date NOT NULL,
     "createdAt" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    height numeric() NOT NULL
+    height numeric(3, 2) NOT NULL
 );
 /* добавьте нового пользователя */
 INSERT INTO users (
@@ -53,11 +53,10 @@ WHERE length(concat("firstName", ' ', "lastName")) > 12;
  Выборка первых 3 записи 
  с ростом от 150 до 180 и возрастом 20+
  */
-SELECT *
+SELECT *,
+    age("birthday")
 FROM "users"
-WHERE height > 1.5
-    AND height < 1.8
-    AND age("birthday") > '20 years'
+WHERE age("birthday") < '25 years'
 LIMIT 3;
 /*  */
 SELECT *
@@ -75,4 +74,67 @@ LIMIT 3;
  с возвратом значений
  (подзапрос)
  */
+DELETE FROM users
+WHERE height > 1.60
+    AND id BETWEEN 80 AND 90
+RETURNING *;
+/*  */
+DELETE FROM users
+WHERE id IN (
+        SELECT id
+        FROM users
+        WHERE height > 1.55
+        LIMIT 2
+    )
+RETURNING *;
+/* 
+ Обновите поле address на значение 'Sobornyi 33' 
+ только тем у кого возраст больше __ 
+ ограничить 5 записями
+ и вернуть значения
+ */
+/* 
+ Обновить 5 записей, кто возрастом от 10 до 20 лет, установить рост 150
+ */
+UPDATE table_name
+SET column1 = value1,
+    column2 = value2
+WHERE id IN (
+        SELECT *
+        FROM users
+        WHERE age("birthday") BETWEEN '10 years' AND '20 years'
+        LIMIT 1
+    );
 /* Найти всех кто родились в сентябре */
+SELECT *
+FROM users
+WHERE date_part('month', "birthday") = 9;
+SELECT *
+FROM users
+WHERE extract(
+        month
+        from "birthday"
+    ) = 9;
+/* 
+ Обновить строки кто родился в январе
+ установить почту на mail@mail.mail
+ */
+UPDATE users
+SET email = 'mail@mail.mail'
+WHERE id IN (
+        SELECT id
+        FROM users
+        WHERE extract(
+                month
+                from "birthday"
+            ) = 1
+        LIMIT 3
+    )
+    RETURNING *;
+/* 
+ column BETWEEN a AND b
+ column IN (cell1, cell2)
+ date_part('part', timestamp) / extract(part from timestamp)
+ SELECT * FROM table LIMIT n;
+ Подзапрос как условие
+ */
