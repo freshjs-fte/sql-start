7f6502258b552d04742ed724c09396324dbe7ad3
Yevhenii <freshjs.fte@gmail.com>
2021-11-03 20:52:37 +0200

    Add aggregate functions tasks

--- fe_db.session.sql
+++ fe_db.session.sql
@@ -1,21 +1,72 @@
+/* Найти самого старого польз */
+SELECT extract(
+        'year'
+        from age("birthday")
+    ) AS "Age",
+    id
+FROM users
+WHERE extract(
+        'year'
+        from age("birthday")
+    ) >= 1
+ORDER BY "birthday" DESC
+LIMIT 1;
+/* найти ид самого младшего пользователя */
+/* найти минимальный возраст по группе пола */
+SELECT min(
+        extract(
+            'year'
+            from age("birthday")
+        )
+    ) AS "Age",
+    "isMale"
+FROM users
+GROUP BY "isMale";
 /* 
- Создайте таблицу “workers” (“id”, “birthday”, “name”, “salary”).
+ Агрегатные функции
+ min
+ max
+ avg - среднее арифметическое
+ count - подсчет
+ 
+ Комбинирование с where и group by
  */
 /* 
- Добавьте двух новых работников одним запросом: Ярослава с зарплатой 
- 1200$ и годом 80го, Петра с зарплатой 1000$ и 93го года.
+ средний рост среди мужчин
  */
+SELECT avg("height") AS "Average height"
+FROM users
+GROUP BY "isMale";
 /* 
- Добавьте нового работника Никиту, 90го года, зарплата 300$.
+ максимальный рост мужчин и женщин
  */
+SELECT max("height") AS "Max height"
+FROM users
+GROUP BY "isMale";
 /* 
- Всем, у кого зарплата ниже 500$ сделайте ее 700$.
+ минимальный, максимальный и средний рост мужчин и женщин
  */
+SELECT max("height") AS "Max height",
+    min("height") AS "Min height",
+    avg("height") AS "Average height"
+FROM users
+GROUP BY "isMale"
+HAVING ;
 /* 
- Выбрать всех, кроме работников с именем Петя.
+ количество людей с определенным именем
  */
+SELECT count("height")
+FROM users
+WHERE "firstName" = 'Leo';
 /* 
- Выбрать всех работников в возрасте от 23 лет до 27 лет или 
- с зарплатой от 400$ до 1000$.
+ количество людей в возрасте от 20 до 30 лет
  */
-/* Выберите всех работников лимит 1 с пагинацией */
+SELECT count(*)
+FROM users
+WHERE date_part('year', age("birthday")) BETWEEN 20 AND 30;
+/* 
+ кол-во людей родившихся 1 января 1970 года
+ */
+ SELECT count(*)
+FROM users
+WHERE "birthday" = '1970-01-01';
