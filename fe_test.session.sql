abc21d1812c6156942414cce1f874104da5f70e3
Yevhenii <freshjs.fte@gmail.com>
2021-11-03 19:37:49 +0200

    Add tasks

--- fe_test.session.sql
+++ fe_test.session.sql
@@ -26,12 +26,6 @@ VALUES (
         '1940.01.01',
         1.5
     );
-/* удалите нового пользователя */
-
-/* RETURNING */
-/* создайте строку и обновите один из ее атрибутов (рост) */
-/* Добавить колонку адрес, добавьте нового пользователя */
-
 /* 
  column BETWEEN a AND b
  column IN (cell1, cell2)
@@ -39,3 +33,44 @@ VALUES (
  SELECT * FROM table LIMIT n;
  Подзапрос как условие
  */
+ /* DML CRUD rows - insert, select, update, delete */
+SELECT *
+FROM users
+WHERE age("birthday") BETWEEN '40 years' AND '50 years'
+LIMIT 50 OFFSET 50 * 2;
+
+SELECT *
+FROM users
+WHERE weight IS NOT NULL;
+
+UPDATE users
+SET weight = 100
+WHERE id % 2 = 1
+RETURNING *;
+
+INSERT INTO users
+(
+    "firstName",
+    "lastName",
+    email,
+    "isMale",
+    birthday,
+    height,
+    weight
+)
+VALUES (
+    'Fedor',
+    'Testov',
+    'mail@mail.mail',
+    true,
+    '2020.01.01',20
+    1.7,
+    NULL
+);
+
+/* найти родившихся в январе или августе или декабре */
+SELECT
+concat("firstName", ' ', "lastName") AS full_name
+FROM users
+WHERE 
+date_part('month', "birthday") IN (1, 8, 12);
