c65d0131ca76fe5037ea55a772bc8c906d6b80b2
Yevhenii <freshjs.fte@gmail.com>
2021-11-05 19:19:54 +0200

    Add relations and basic joins

--- sql/relation_algebra.sql
+++ sql/relation_algebra.sql
@@ -35,32 +35,32 @@ FROM b;
 
 
 
+/* Джоин антипатерн, не использовать */
+SELECT * FROM a, b
+WHERE a.value = b.v2;
+
+/* Бaзовый JOIN - связь таблиц не обязательна */
+SELECT * FROM a
+    JOIN b ON a.value = b.v2;/* FK -> PK */
+
+/* Кол-во заказов на каждого польз */
+SELECT 
+count(*) "Count orders", u.id "User ID", u.email
+FROM orders o
+  JOIN users u ON o."userId" = u.id
+GROUP BY "User ID";
 
 
+/* все заказы бренда Samsung */
+SELECT pto."orderId", p.brand 
+FROM phones_to_orders pto
+    JOIN phones p ON pto."phoneId" = p.id
+WHERE p.brand ~* 'SamsuNg';
 
+/* кол-во моделей бренда Samsung в заказах  */
+SELECT pto."orderId", p.brand, count(*) "Count models"
+FROM phones_to_orders pto
+    JOIN phones p ON pto."phoneId" = p.id
+WHERE p.brand ~* 'SamsuNg'
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*  */
-INSERT INTO orders ("userId")
-VALUES (4),
-    (6),
-    (1),
-    (9);
-/*  */
-SELECT users."id",
-    users."firstName",
-    users."lastName",
-    orders."id" AS "order id"
-FROM users
-    JOIN orders ON orders."userId" = users."id";
+GROUP BY pto."orderId", p.brand;
--- sql/relations.sql
+++ sql/relations.sql
@@ -1,4 +1,4 @@
-
+DROP TABLE users;
 CREATE TABLE users (
     id serial PRIMARY KEY,
     "firstName" varchar(64) NOT NULL,
@@ -10,7 +10,8 @@ CREATE TABLE users (
     height numeric(3, 2) NOT NULL,
     weight int
 );
-
+/*  */
+DROP TABLE phones;
 CREATE TABLE phones (
     id serial PRIMARY KEY,
     model varchar(64) NOT NULL CHECK(model != ''),
@@ -18,4 +19,17 @@ CREATE TABLE phones (
     price numeric(8, 2) NOT NULL CHECK (price >= 100),
     stock_quantity INT NOT NULL CHECK (stock_quantity >= 1),
     UNIQUE (brand, model)
+);
+/* 1 user : N orders */
+CREATE TABLE orders (
+    id serial PRIMARY KEY,
+    user_id int REFERENCES users(id)
+    /* FK -> PK */
+);
+/* 1 phone : N orders */
+/* 1 order : N phones */
+CREATE TABLE phones_to_orders(
+    order_id int REFERENCES orders(id),
+    phone_id int REFERENCES phones(id),
+    UNIQUE(order_id, phone_id)
 );
